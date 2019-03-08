03163ed5fa9bc299b4de5a77a9a7bf5c6f3df2c8
adwait1-g <adwait.gautham@gmail.com>
2019-03-09 02:10:16 +0530

    Wrote code to cover all corner cases

--- Assignment 1/src/AVL/problem1.cpp
+++ Assignment 1/src/AVL/problem1.cpp
@@ -8,15 +8,26 @@ using namespace avltree;
 
 int main(int argc, char **argv) {
 
-    Node* root1 = populateTree(NULL, 1);
-    Node* root2 = populateTree(NULL, 2);
+    Node* root1 = NULL;
+    root1 = populateTree(NULL, 1);
+
+    Node* root2 = NULL;
+    root2 = populateTree(NULL, 2);
 
     std::cout<<"Tree-1: "<<std::endl;
-    displayTree(root1);
+    if(root1 != NULL)
+        displayTree(root1);
+    else
+        std::cout<<"No T1 exists"<<std::endl;
+
     std::cout<<"\n"<<std::endl;
 
     std::cout<<"Tree-2: "<<std::endl;
-    displayTree(root2);
+    if(root2 != NULL)
+        displayTree(root2);
+    else
+        std::cout<<"No T2 exists"<<std::endl;
+    
     std::cout<<"\n"<<std::endl;
 
     std::cout<<"Merging trees...."<<std::endl;
@@ -24,7 +35,14 @@ int main(int argc, char **argv) {
     Node *root = mergeTrees(root1, root2);
 
     std::cout<<"Final merged tree: "<<std::endl;
-    displayTree(root);
+    if(root != NULL)
+        displayTree(root);
+    else {
+        std::cout<<"No tree exists because there were no T1 and T2"<<std::endl;
+        return 0;
+    }
+
+    
     std::cout<<"\n"<<std::endl;
     std::cout<<"Inorder: "<<std::endl;
     inorder(root);
