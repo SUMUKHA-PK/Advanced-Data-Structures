75baa60289a7d79eb740f3c30aa20d3daca11895
Sumukha Pk <sumukhapk46@gmail.com>
2019-03-10 03:54:00 +0530

    Some progress in heapifying

--- Assignment 2/src/BinomialHeap.cpp
+++ Assignment 2/src/BinomialHeap.cpp
@@ -8,25 +8,25 @@ int main(){
     Node* node= createNode(10,hashMap);
     //Look into the problem of returning nodes
 
-    node = heapUnion(node,node,createNode(20,hashMap),hashMap);
+    node = heapUnion(&node,node,createNode(20,hashMap),hashMap);
     printHeap(node);
 
-    node = heapUnion(node,node,createNode(2,hashMap),hashMap);
+    node = heapUnion(&node,node,createNode(2,hashMap),hashMap);
     printHeap(node);
 
-    node = heapUnion(node,node,createNode(11,hashMap),hashMap);
+    node = heapUnion(&node,node,createNode(11,hashMap),hashMap);
     printHeap(node);
     
-    node = heapUnion(node,node,createNode(1,hashMap),hashMap);
+    node = heapUnion(&node,node,createNode(1,hashMap),hashMap);
     printHeap(node);
 
-    node = heapUnion(node,node,createNode(3,hashMap),hashMap);
+    node = heapUnion(&node,node,createNode(3,hashMap),hashMap);
     printHeap(node);
 
-    node = heapUnion(node,node,createNode(5,hashMap),hashMap);
+    node = heapUnion(&node,node,createNode(5,hashMap),hashMap);
     printHeap(node);
 
-    node = heapUnion(node,node,createNode(7,hashMap),hashMap);
+    node = heapUnion(&node,node,createNode(7,hashMap),hashMap);
     printHeap(node);
 
     return 0;
--- Assignment 2/src/BinomialHeap.h
+++ Assignment 2/src/BinomialHeap.h
@@ -13,6 +13,12 @@ namespace binomialheap{
         struct node* right = NULL;
     }Node;
 
+/*
+ *  createNode creates a node* pointer,
+ *  checks whether a single node exists,
+ *  and hashes that values in if necessary.
+ */
+
     Node * createNode(int n,map<int,int> &hashMap){
         Node * node = new Node;
         node->val = n;
@@ -48,14 +54,24 @@ namespace binomialheap{
         cout<<endl;
     }
 
-    void fixHeap(Node* heapNode,map<int,int> &hashMap,int l){
-        Node* temp = heapNode;
+/*
+ *  fixHeap : Used to make the structure of the heap bheap type.
+ *  calledBy -> heapUnion.
+ *  hashmap is used to hash the values and check whether there is a collision,
+ *  and heap fixing is needed.
+ */
+
+    void fixHeap(Node** heapNode,map<int,int> &hashMap,int l){
+        Node* temp = *heapNode;
         cout<<l<<endl;
         while(temp->l<l){
             temp=temp->right;
         }
         if(temp->l!=l) temp=temp->right;
         if(temp->val<temp->right->val){
+            // if(temp==*heapNode){
+            //     *heapNode = temp->right;
+            // }
             temp->nodes.push_back(temp->right);
             temp->right->parent=temp;
             // temp->right->left=NULL;
@@ -70,10 +86,14 @@ namespace binomialheap{
                 hashMap.insert(pair<int,int>(temp->l,1));
             }
             else{
-                fixHeap(temp,hashMap,temp->l);
+                fixHeap(heapNode,hashMap,temp->l);
             }
         }
         else{
+            int g=0;
+            if(temp==*heapNode){
+                g=1;
+            }
             temp->right->nodes.push_back(temp);
             temp->parent=temp->right;
             if(temp->left!=NULL){
@@ -82,25 +102,33 @@ namespace binomialheap{
             }
             else{
                 temp->right->left=NULL;
+                // temp->right=NULL;
+                // temp->left=NULL;
             }
-            temp->left=NULL;
-            // temp->right=NULL;
-            temp=temp->right;
+            temp=t->right;
             hashMap.erase(temp->l);
             temp->l = temp->l + 1;
             if(hashMap.find(temp->l)==hashMap.end()){
                 hashMap.insert(pair<int,int>(temp->l,1));
             }
             else{
-                fixHeap(temp,hashMap,temp->l);
+                fixHeap(heapNode,hashMap,temp->l);
             }
         }
     }
 
-    Node * heapUnion(Node* node,Node* node1, Node* node2, map<int,int> &hashMap){  // Add node2 heap to node1 heap
+/*
+ *  heapUnion: Merges 2 heaps.
+ *  Adds node2 heap to node1 heap.
+ *  Checks whether there is a collision and then calls fixHeap.
+ */
+
+    Node * heapUnion(Node** node,Node* node1, Node* node2, map<int,int> &hashMap){  
+        //Iterate to find the right node in order to insert
         while(node1->right!=NULL && node1->l<node2->l){
             node1=node1->right;
         }
+        // If the degrees of the nodes are same, compare the value and add based on that
         if(node1->l==node2->l){
             if(node1->val<node2->val){
                 node1->nodes.push_back(node2);
@@ -108,6 +136,27 @@ namespace binomialheap{
                 node2->head=0;   
                 hashMap.erase(node1->l);
                 node1->l= node1->l+1;
+               if(hashMap.find(node1->l)==hashMap.end()){
+                    hashMap.insert(pair<int,int>(node1->l,1));
+                }
+                // If there is a collision in the 'l' values, fixheap.
+                else{
+                    fixHeap(node,hashMap,node1->l);
+                }
+            }
+            else{
+                node2->right=node1->right;
+                node2->left=node1->left;
+                node1->left=NULL;
+                node1->right=NULL;
+                node2->nodes.push_back(node1);
+                node1->parent=node2;
+                node1->head=0;
+                node2->head=1;
+                hashMap.erase(node1->l);
+                node2->l= node2->l+1;
+                node1=node2;
+                *node = node1;
                 if(hashMap.find(node1->l)==hashMap.end()){
                     hashMap.insert(pair<int,int>(node1->l,1));
                 }
@@ -115,20 +164,8 @@ namespace binomialheap{
                     fixHeap(node,hashMap,node1->l);
                 }
             }
-            else{
-                node2->nodes.push_back(node1);
-                node2->parent=node1;
-                node1->head=0;
-                hashMap.erase(node1->l);
-                node2->l= node2->l+1;
-                if(hashMap.find(node2->l)==hashMap.end()){
-                    hashMap.insert(pair<int,int>(node2->l,1));
-                }
-                else{
-                    fixHeap(node,hashMap,node2->l);
-                }
-            }
         }
+        // If the degree is not same, insert checking whether its the first node to be added or in the middle.
         else{
             if(node1->left==NULL){
                 node2->right=node1;
