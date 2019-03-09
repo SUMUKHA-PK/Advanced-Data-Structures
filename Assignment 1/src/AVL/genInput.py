e14f09fce96c08b17e401d344b25db56e144ad31
adwait1-g <adwait.gautham@gmail.com>
2019-03-09 09:49:54 +0530

    Started input generating script

---
+++ Assignment 1/src/AVL/genInput.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+import rand
+
+def problem1() : 
+
+    n1 = int(input("Enter number of nodes in T1: "))
+    n2 = int(input("Enter number of nodes in T2: "))
+
+    
+
+
+def problem2() : 
+
+
+
+if __name__ == "__main__" : 
+
+    if(len(sys.argv) != 2) {
+        print("Usage: $", sys.argv[0], " <problem1 / problem2>")
+        sys.exit()
+    }
+
+    if sys.argv[1] == "problem1" : 
+        problem1()
+    
+    elif sys.argv[2] == "problem2": 
+        problem2()
